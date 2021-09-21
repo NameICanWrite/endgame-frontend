63c467ada5c681c6934fbe1588a2d965f1cb3218
Vadim <vadimbaranivsky83@gmail.com>
2021-09-21 15:56:31 +0300

    &

--- src/components/userProfile/API.js
+++ src/components/userProfile/API.js
@@ -1,12 +1,6 @@
 import axios from 'axios';
-<<<<<<< HEAD
 import { path } from '../../App';
 // const path = 'https://endgame-backend.herokuapp.com'
-=======
-
-// const path = 'https://endgame-backend.herokuapp.com'
-const path = 'http://localhost:5000'
->>>>>>> 5f19d49cdd19084929351ba97c1fd300a159caeb
 
 export const fetchUserData = async () => {
     const response = await axios.get(path + '/users/user-data', {withCredentials: true});
@@ -15,19 +9,9 @@ export const fetchUserData = async () => {
 }
 
 export const postUserData = async (data) => {
-<<<<<<< HEAD
     const response = await axios.post(path + '/users/user-data', data, {withCredentials: true})
     .catch(err => {
       throw new Error(err.response.data)
     })
-=======
-    console.log(data)
-    const response = await axios({
-        method:  'POST',
-        url: path + '/users/user-data',
-        data,
-        withCredentials: true
-      });
->>>>>>> 5f19d49cdd19084929351ba97c1fd300a159caeb
     return response.data
 }
